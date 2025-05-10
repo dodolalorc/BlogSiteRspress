a38700bb87220c25e84632617150c43b4329a60e
Flora Chen <chenflora124@gmail.com>
2025-05-10 21:18:36 +0800

    update

--- rspress.config.ts
+++ rspress.config.ts
@@ -10,9 +10,29 @@ export default defineConfig({
 		light: "/icon.png",
 		dark: "/icon.png",
 	},
-	mediumZoom: {
-		selector: ".rspress-doc img",
-	},
+	builderConfig: {
+    html: {
+      tags: [
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'stylesheet',
+						href: 'https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.min.css',
+						integrity: "sha384-5TcZemv2l/9On385z///+d7MSYlvIEw9FuZTIdZ14vJLqWphw7e7ZPuOiCHJcFCP",
+						crossorigin: "anonymous"
+          },
+        },
+        {
+          tag: 'script',
+          attrs: {
+						src: 'https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/contrib/auto-render.min.js',
+						integrity: "sha384-hCXGrW6PitJEwbkoStFjeJxv+fSOOQKOPbJxSfM6G5sWZjAyWhXiTIIAmQqnlLlh", crossorigin: "anonymous",
+						onload: "renderMathInElement(document.body);"
+					},
+        },
+      ],
+    },
+  },
 	plugins: [pluginLastUpdated()],
 	route: {
 		exclude: [],
