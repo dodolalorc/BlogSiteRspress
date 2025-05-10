6ce71490ddb13d66132c16905464c223079aa672
Flora Chen <chenflora124@gmail.com>
2025-05-10 16:31:41 +0800

    biome files

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,30 +1,31 @@
 import * as path from "node:path";
+import { pluginLastUpdated } from "@rspress/plugin-last-updated";
+import { pluginPlayground } from "@rspress/plugin-playground";
 import { defineConfig } from "rspress/config";
-import { pluginPlayground } from '@rspress/plugin-playground';
-
 
 export default defineConfig({
-  root: path.join(__dirname, "docs"),
-  title: "Dodolalorc",
-  icon: "/icon.png",
-  logo: {
-    light: "/icon.png",
-    dark: "/icon.png",
-  },
-  plugins: [
-    pluginPlayground(),
-  ],
-  route: {
-    exclude: [],
-  },
-  globalStyles: path.join(__dirname, 'src', 'styles', 'index.css'),
-  themeConfig: {
-    socialLinks: [
-      {
-        icon: "github",
-        mode: "link",
-        content: "https://github.com/dodolalorc",
-      },
-    ],
-  },
+	root: path.join(__dirname, "docs"),
+	title: "Dodolalorc",
+	icon: "/icon.png",
+	logo: {
+		light: "/icon.png",
+		dark: "/icon.png",
+	},
+	mediumZoom: {
+		selector: ".rspress-doc img",
+	},
+	plugins: [pluginPlayground(), pluginLastUpdated()],
+	route: {
+		exclude: [],
+	},
+	globalStyles: path.join(__dirname, "src", "styles", "index.css"),
+	themeConfig: {
+		socialLinks: [
+			{
+				icon: "github",
+				mode: "link",
+				content: "https://github.com/dodolalorc",
+			},
+		],
+	},
 });
